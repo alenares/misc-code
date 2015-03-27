4364e2cbfb76c2e9d3dd9c07510027113028d588
Alessandro Lenares <alenares@users.noreply.github.com>
2015-03-27 15:01:58 -0400

    Create sort_dictionary.py

---
+++ sort_dictionary.py
@@ -0,0 +1,14 @@
+
+'''
+  tags: dictionary sort
+'''
+
+import operator
+
+# sort dictionary by value
+x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+sorted_x = sorted(x.items(), key=operator.itemgetter(1))
+
+# sort dictionary by key
+x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+sorted_x = sorted(x.items(), key=operator.itemgetter(0))
